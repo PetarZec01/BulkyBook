a9b81f68febfbd611d75709230ae97216fe6b886
Petar Zec <63071277+PetarZec01@users.noreply.github.com>
2022-12-18 12:29:20 +0100

    Added the Category page, connected the database and made the first MVC of the Category display and functionality

---
+++ BulkyBook/Controllers/CategoryController.cs
@@ -0,0 +1,27 @@
+﻿using BulkyBook.Data;
+using BulkyBook.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBook.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDBContext _db;
+
+        public CategoryController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoriesList = _db.Categories;
+            return View(objCategoriesList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
